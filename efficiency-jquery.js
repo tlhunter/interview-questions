40b844b6276c2e4953fc884c53f0aa50ea7d5eb4
Thomas Hunter II <thunter@opentable.com>
2016-01-25 15:33:48 -0800

    typo, clarification

--- efficiency-jquery.js
+++ efficiency-jquery.js
@@ -2,7 +2,7 @@ var $c = $('#collection');
 
 $c.css('color', 'red');
 
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 1000; i++) {
   $('#collection .item-' + i).text("This is Item #" + i);
 }
 
