0ace3c34e257aa8707a2bb63329bc5ff4752b597
Thomas Hunter II <thunter@opentable.com>
2016-04-06 11:08:28 -0700

    vanilla JS version of jQuery

--- efficiency-jquery.js
+++ efficiency-jquery.js
@@ -1,10 +1,13 @@
 var $c = $('#collection');
+// var $c = document.getElementById('collection');
 
 $c.css('color', 'red');
+// $c.style.color = 'red';
 
 for (var i = 0; i < 1000; i++) {
   // Can you make this query more efficient?
   $('#collection .item-' + i).text("This is Item #" + i);
+  // document.querySelector('#collection .item-' + i).innerHTML = i;
 }
 
 // <div id="collection">
