b11663c6dc85a2c5758f75909bbd111ad691b7d7
Thomas Hunter II <me@thomashunter.name>
2021-08-24 10:14:05 -0700

    Update db-auth.js

--- db-auth.js
+++ db-auth.js
@@ -1,21 +1,21 @@
 // Please fix the bug with this controller code
 
-var database = require('database_connection.js');
-var encrypt = require('sha1_encryptor.js');
+const database = require('database_connection.js');
+const encrypt = require('sha1_encryptor.js');
 
-app.post('/login', (req, res) => { // Triggered by event, could be $('el').click
-  var username = req.body.username;
-  var password = req.body.password;
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
   res.authorized = true;
   res.status = 200;
 
-  database.find('users', username, (err, user) => { // Async operation, could be $.ajax
+  database.find('users', username, (err, user) => {
     if (err || encrypt(password) !== user.password) {
       res.status = 401;
       res.authorized = false;
     }
   });
 
-  res.send(); // Sync operation, could be $('el').css('color', 'red');
+  res.send();
 });
